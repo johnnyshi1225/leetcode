1f27619fda5e0d35a686b71e2e80d5572a8f71e1
johnnyshi1225 <841471958@qq.com>
2018-08-25 22:50:31 +0800

    learning

---
+++ learn/array_and_string/reverse_string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# Author: Johnny Shi
+# Created Time: 2018-08-25 00:53:16
+# File Name: reverse_string.py
+# Description:
+#########################################################################
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ''
+        array_s = list(s)
+        array_s.reverse()
+        return ''.join(array_s)
+
+s = '123'
+s = 'A man, a plan, a canal: Panama'
+ret = Solution().reverseString(s)
+print(ret)
+
+# vim: set expandtab ts=4 sts=4 sw=4 :
