15c3ddc61d2f8c03db09d22b516a8e66c28014fb
johnnyshi1225 <841471958@qq.com>
2018-08-25 23:39:11 +0800

    learn => problems

---
+++ problems/27_remove_element.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# Author: Johnny Shi
+# Created Time: 2018-08-25 01:36:47
+# File Name: remove_element.py
+# Description:
+#########################################################################
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
+        return i
+
+nums = [3, 2, 3, 2, 1]
+val = 3
+ret = Solution().removeElement(nums, val)
+print(ret)
+print(nums)
+
+# vim: set expandtab ts=4 sts=4 sw=4 :
---
+++ problems/498_diagonal_traverse.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# Author: Johnny Shi
+# Created Time: 2018-08-24 14:19:16
+# File Name: diagonal_traverse.py
+# Description:
+#########################################################################
+class Solution:
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix:
+            return []
+        m = len(matrix)
+        n = 0
+        if m:
+            n = len(matrix[0])
+        else:
+            return []
+
+        ret = [0] * (m * n)
+        i = 1
+        j = -1
+        next_offset = [(-1, 1), (1, -1)]
+        cur_next = 0
+        for index in range(0, m * n):
+            # move to next
+            i += next_offset[cur_next][0]
+            j += next_offset[cur_next][1]
+
+            if j >= n:
+                # 上角
+                i += 2
+                j = n - 1
+                cur_next = 1
+            elif i >= m:
+                # 下角
+                i = m - 1
+                j += 2
+                cur_next = 0
+            elif i < 0:
+                i = 0
+                cur_next = 1
+            elif j < 0:
+                j = 0
+                cur_next = 0
+            print(index, i, j)
+            ret[index] = matrix[i][j]
+
+        return ret
+
+
+matrix = [[]]
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+matrix = [
+    [1, 2],
+    [3, 4]
+]
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+ret = Solution().findDiagonalOrder(matrix)
+print(ret)
+
+# vim: set expandtab ts=4 sts=4 sw=4 :
