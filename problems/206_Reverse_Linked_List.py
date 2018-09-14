fba6b72bce748d46d56f7d2fc9c5b0bec7804e10
johnnyshi1225 <sxc1225@gmail.com>
2018-09-14 13:53:35 +0800

    21 new; improve 206 234

--- problems/206_Reverse_Linked_List.py
+++ problems/206_Reverse_Linked_List.py
@@ -7,11 +7,14 @@
 # Description:
 #########################################################################
 from simple_linked_list import linked_list, print_linked_list
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution:
     # not in-place
@@ -38,9 +41,16 @@ class Solution:
         :type head: ListNode
         :rtype: ListNode
         """
+        new_head = None
+        while head:
+            tmp = head.next
+            head.next = new_head
+            new_head = head
+            head = tmp
+        return new_head
 
 
 head = linked_list([1, 2, 3, 4])
-ret = Solution().reverse(head)
+ret = Solution().reverseList(head)
 print_linked_list(ret)
 # vim: set expandtab ts=4 sts=4 sw=4 :
--- problems/234_Palindrome_Linked_List.py
+++ problems/234_Palindrome_Linked_List.py
@@ -6,7 +6,7 @@
 # File Name: 234_Palindrome_Linked_List.py
 # Description:
 #########################################################################
-from simple_linked_list import linked_list, print_linked_list
+from simple_linked_list import linked_list
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -15,7 +15,7 @@ from simple_linked_list import linked_list, print_linked_list
 
 
 class Solution:
-    def isPalindrome(self, head):
+    def isPalindrome1(self, head):
         """
         :type head: ListNode
         :rtype: bool
@@ -35,10 +35,37 @@ class Solution:
                 return False
         return True
 
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        # 通过快慢指针，找到中点位置
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        if fast:
+            slow = slow.next
+        # reverse from current slow
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+        while prev:
+            if prev.val == head.val:
+                prev = prev.next
+                head = head.next
+            else:
+                return False
+        return True
 
 
-head = linked_list([1, 2, 2, 1])
+head = linked_list([1, 2, 3, 2, 1])
 head = linked_list([1, 2, 3, 4])
+head = linked_list([1, 2, 2, 1])
 ret = Solution().isPalindrome(head)
 print(ret)
 # vim: set expandtab ts=4 sts=4 sw=4 :
