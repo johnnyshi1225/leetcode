a4895c816a40fdd65c86ce504c533d38860f4b01
johnnyshi1225 <sxc1225@gmail.com>
2018-09-21 12:56:18 +0800

    136

---
+++ problems/136_Single_Number.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# Author: Johnny Shi
+# Created Time: 2018-09-21 12:42:01
+# File Name: 136_Single_Number.py
+# Description:
+#########################################################################
+
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hset = set()
+        for n in nums:
+            if n in hset:
+                hset.remove(n)
+            else:
+                hset.add(n)
+        if hset:
+            return hset.pop()
+
+
+nums = [1, 2, 3, 2, 1]
+print(Solution().singleNumber(nums))
+# vim: set expandtab ts=4 sts=4 sw=4 :
