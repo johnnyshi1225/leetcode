7404d6d9eef55de206b3a7ac6eca4dfd18f80f48
johnnyshi1225 <sxc1225@gmail.com>
2018-09-24 10:50:31 +0800

    599 better way

--- problems/599_Minimum_Index_Sum_of_Two_Lists.py
+++ problems/599_Minimum_Index_Sum_of_Two_Lists.py
@@ -6,6 +6,7 @@
 # File Name: 599_Minimum_Index_Sum_of_Two_Lists.py
 # Description:
 #########################################################################
+import sys
 
 
 class Solution:
@@ -16,22 +17,23 @@ class Solution:
         :rtype: List[str]
         """
         hmap = {}
-        ret_map = {}
+        min_sum = sys.maxsize
+        min_list = []
         for i in range(len(list1)):
             hmap[list1[i]] = i
         for i in range(len(list2)):
             if list2[i] in hmap:
                 isum = i + hmap[list2[i]]
-                if isum not in ret_map:
-                    ret_map[isum] = [list2[i]]
-                else:
-                    ret_map[isum].append(list2[i])
-        isums = list(ret_map.keys())
-        isums.sort()
-        return ret_map[isums[0]]
+                if isum < min_sum:
+                    min_sum = isum
+                    min_list = [list2[i]]
+                elif isum == min_sum:
+                    min_list.append(list2[i])
+        return min_list
 
 
 list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+list1 = ["Shogun", "KFC", "Tapioca Express", "Burger King"]
 list2 = ["KFC", "Shogun", "Burger King"]
 print(Solution().findRestaurant(list1, list2))
 # vim: set expandtab ts=4 sts=4 sw=4 :
