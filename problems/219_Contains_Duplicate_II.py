e4dfd996fac5856c3745ba430eb1062d474dde45
johnnyshi1225 <sxc1225@gmail.com>
2018-09-26 13:00:37 +0800

    219 better solution

--- problems/219_Contains_Duplicate_II.py
+++ problems/219_Contains_Duplicate_II.py
@@ -9,7 +9,7 @@
 
 
 class Solution:
-    def containsNearbyDuplicate(self, nums, k):
+    def containsNearbyDuplicate1(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -21,21 +21,32 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] in hmap:
                 hmap[nums[i]].append(i)
+                if hmap[nums[i]][-1] - hmap[nums[i]][-2] <= k:
+                    return True
             else:
                 hmap[nums[i]] = [i]
-        for _, v in hmap.items():
-            if len(v) > 1:
-                i = 0
-                while i < len(v) - 1:
-                    if v[i + 1] - v[i] <= k:
-                        return True
-                    i += 1
+        return False
+
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        hmap = {}
+        for i, n in enumerate(nums):
+            if n in hmap:
+                if i - hmap[n] <= k:
+                    return True
+            hmap[n] = i
         return False
 
 
-nums = [1, 2, 3, 1]
-k = 3
 nums = [1, 0, 1, 1]
 k = 1
+nums = [1, 2, 3, 1]
+k = 3
 print(Solution().containsNearbyDuplicate(nums, k))
 # vim: set expandtab ts=4 sts=4 sw=4 :
